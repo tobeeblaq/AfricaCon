4ef8e8a28502718e352a438fae3286372337fb63
tobbeblaq <ogunstobi@gmail.com>
2018-12-20 09:32:00 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LandPageComponent } from './land-page/land-page.component';
+import { PostpageComponent } from './postpage/postpage.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandPageComponent,
+    PostpageComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
